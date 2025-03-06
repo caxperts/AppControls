ec38efdb08d5eba1db55beb6e9860894b617f68f
e-ruhland <e.ruhland@caxperts.com>
2025-03-06 10:44:33 +0100

    Remove Playground/Migrate samples to samples

--- playground/README.md
+++
@@ -1,3 +0,0 @@
-# AppControls Playground
-These are the source files behind the hosted version of the [AppControls Playground](https://playground.universalplantviewer.com).
-This site only work beginning with UPV 7.1.0 (not yet released). Till then only Sample 1-3 are working
--- samples/03_inputfield/src/Content.tsx
+++ samples/03_inputfield/src/Content.tsx
@@ -1,26 +1,57 @@
 import Stack from '@mui/material/Stack';
 import { Button, MenuItem, TextField, Typography } from '@mui/material';
 import { useRef } from 'react';
-import { Application } from '@caxperts/universal.api';
+import { Application, AttributeConditionComparison, CombineModes, ConsolidationMode, PackageCondition } from '@caxperts/universal.api';
 
 
 function Content() {
 
     // We define a reference to a HTMLInputElement. This will be later bound and used to access the value of the input field
-    const conditionRef = useRef<HTMLInputElement>()
-    const actionRef = useRef<HTMLSelectElement>()
+    const conditionRef = useRef<HTMLInputElement>();
+    const actionRef = useRef<HTMLSelectElement>();
+    const combineModeRef = useRef<HTMLSelectElement>();
+    const packageFilterRef = useRef<HTMLSelectElement>();
 
     async function execute() {
         // Check if we are running inside UPV
-        const runningInUPV = Application.getInstance().available()
-        console.log(runningInUPV)
+        const runningInUPV = Application.getInstance().available();
+        console.log(runningInUPV);
 
         // get a 3D scene element
-        const scene = (await Application.getInstance().Scenes3d.get())[0]
+        const scene = (await Application.getInstance().Scenes3d.get())[0];
         // get a new filter
         const filter = scene.getNewFilter();
-        // Set the condition of the filter based on the value of the text field
+        // Set the condition of the filter based on the value of the text field the & symbol can be used to combine multiple queries. THe combinemode defines how they are combined.
         filter.Condition = conditionRef.current?.value;
+
+        // Based on the action value we either select, fit or highlight
+        switch (combineModeRef.current?.value) {
+            case "And":
+                filter.CombineMode = CombineModes.And;
+                break;
+            case "Or":
+                filter.CombineMode = CombineModes.Or;
+                break;
+        }
+        
+        switch (combineModeRef.current?.value) {
+            // if we say yes we will peform the action using this Package filter. This is applied on top of the condition and combinemode.
+            case "Yes":
+                filter.APIPackageFilter = {
+                    Name: "APIFilter",
+                    Conditions: [
+                        // Every first Condition is a base Condition that the other ones are based upon
+                        // In this condition we create the condition Area System = Sulphur Recovery Area-A
+                        PackageCondition.createAttributeCondition(ConsolidationMode.Base, "Area System", AttributeConditionComparison.Equals, "Sulphur Recovery Area-A"),
+                        // The second condition extends the first by defining that Task should be Equipment-
+                        PackageCondition.createAttributeCondition(ConsolidationMode.And, "Task", AttributeConditionComparison.Equals, "Equipment"),
+                        // The last condition states stat the Name cannot be D-240
+                        PackageCondition.createAttributeCondition(ConsolidationMode.AndNot, "Name", AttributeConditionComparison.Equals, "D-240"),
+                        //Various other conditions are also possible. Live VolumeConditions or groups / IntelliGroups
+                    ]
+                }
+                break;
+        }
 
         // Based on the action value we either select, fit or highlight
         switch (actionRef.current?.value) {
@@ -31,6 +62,12 @@ function Content() {
                 await filter.fit();
                 break;
             case "Highlight":
+                await filter.highlight();
+                break;
+            case "All":
+                //The can be called one after the other to perform all the actions
+                await filter.select();
+                await filter.fit();
                 await filter.highlight();
                 break;
         }
@@ -53,6 +90,19 @@ function Content() {
                     <MenuItem value="Select">Select</MenuItem>
                     <MenuItem value="Fit">Fit</MenuItem>
                     <MenuItem value="Highlight">Highlight</MenuItem>
+                    <MenuItem value="All">All</MenuItem>
+                </TextField>
+                { /* Create a Textfield that acts as a select element. There is also a Select MUi element that allows more customisation */}
+                <TextField label='Condition Combine Mode' variant='filled' defaultValue={"And"} select inputRef={combineModeRef}>
+                    {/* Create 3 Menuitems for the actions we want to perform */}
+                    <MenuItem value="And">And</MenuItem>
+                    <MenuItem value="Or">Or</MenuItem>
+                </TextField>
+                { /* Create a Textfield that acts as a select element. There is also a Select MUi element that allows more customisation */}
+                <TextField label='Use Package Filter (defined in code)' variant='filled' defaultValue={"No"} select inputRef={packageFilterRef}>
+                    {/* Create 3 Menuitems for the actions we want to perform */}
+                    <MenuItem value="No">No</MenuItem>
+                    <MenuItem value="Yes">Yes</MenuItem>
                 </TextField>
                 { /* Define the MUI Button that calls our function if clicked */}
                 <Button variant='contained' onClick={execute}>Run Action</Button>
